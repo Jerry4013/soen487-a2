fe8d87d37c3c605bebe863b4d15704886338eced
Jerry4013 <jingchao.zhang@mail.concordia.ca>
2020-04-01 02:52:13 -0400

    config

--- library-client/src/environments/environment.prod.ts
+++ library-client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://fathomless-savannah-53408.herokuapp.com/',
+  baseUrl: '',
   book: '/book',
   list: '/list',
   auth: '/auth',
